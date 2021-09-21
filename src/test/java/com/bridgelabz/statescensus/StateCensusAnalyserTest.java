4dac1d3ddfaafb0f7e09fa5d4e2c38026c72b6d4
Swathi <swathi.h@perfios.com>
2021-09-21 20:12:11 +0530

    Test cases to throw custom exception when incorrect input given

---
+++ src/test/java/com/bridgelabz/statescensus/StateCensusAnalyserTest.java
@@ -0,0 +1,101 @@
+package com.bridgelabz.statescensus;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import com.bridgelabz.statescensus.CensusAnalyserException;
+import com.bridgelabz.statescensus.StateCensusAnalyser;
+
+public class StateCensusAnalyserTest {
+
+	public static final String CSV_FILE_PATH = "./StateCensus.csv";
+	public static final String CSV_STATECODE_PATH = "./StateCode.csv";
+	public static final String NO_FILE = "./State.csv";
+	public static final String WRONG_FILE = "./State.json";
+
+	@Test
+	public void GivenStateCensusCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() {
+
+		int count = StateCensusAnalyser.loadDataToIterator(CSV_FILE_PATH, CSVStateCensus.class);
+		Assert.assertEquals(29, count);
+
+	}
+
+	@Test
+	public void GivenStateCensusCsvFile_IfInCorrect_ShouldThrowException() {
+		try {
+			StateCensusAnalyser.loadDataToIterator(NO_FILE, CSVStateCensus.class);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.NO_SUCH_FILE, e.type);
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void GivenStateCensusCSVFile_TypeIncorrect_ReturnsCensusAnalyserException() {
+		try {
+			StateCensusAnalyser.loadDataToIterator(WRONG_FILE, CSVStateCensus.class);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.TYPE_INCORRECT, e.type);
+			System.out.println(e.getMessage());
+		}
+	}
+
+	@Test
+	public void GivenStateCensusCSVFile_WithDelimiterIncorrect_ReturnsCensusAnalyserException() {
+		try {
+			StateCensusAnalyser.loadDataToIterator(CSV_FILE_PATH, CSVStateCensus.class);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.TYPE_INCORRECT, e.type);
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void GivenStateCensusCSVFile_IncorrectHeader_ReturnsCensusAnalyserException() {
+		try {
+			StateCensusAnalyser.loadDataToIterator(CSV_FILE_PATH, CSVStateCensus.class);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.TYPE_INCORRECT, e.type);
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void GivenStateCodeCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() {
+
+		int count = StateCensusAnalyser.loadDataToIterator(CSV_STATECODE_PATH, CSVStateCode.class);
+		Assert.assertEquals(37, count);
+
+	}
+	
+	@Test
+	public void GivenStateCodeCsvFile_IfInCorrect_ShouldThrowException() {
+		try {
+			StateCensusAnalyser.loadDataToIterator(NO_FILE, CSVStateCensus.class);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.NO_SUCH_FILE, e.type);
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void GivenStateCodeCSVFile_TypeIncorrect_ReturnsCensusAnalyserException() {
+		try {
+			StateCensusAnalyser.loadDataToIterator(WRONG_FILE, CSVStateCensus.class);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.TYPE_INCORRECT, e.type);
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void GivenStateCodeCSVFile_WithDelimiterIncorrect_ReturnsCensusAnalyserException() {
+		try {
+			StateCensusAnalyser.loadDataToIterator(CSV_STATECODE_PATH, CSVStateCensus.class);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.TYPE_INCORRECT, e.type);
+			System.out.println(e.getMessage());
+		}
+	}
+}
