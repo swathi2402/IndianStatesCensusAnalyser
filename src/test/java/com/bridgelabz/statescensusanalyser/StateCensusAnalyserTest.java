3fcc2a7620d616c0434419ef4e1d3fc63ffd22e7
Swathi <swathi.h@perfios.com>
2021-09-21 19:03:24 +0530

    TC1.5: Test to give custom exception when incorrect header given

--- src/test/java/com/bridgelabz/statescensusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statescensusanalyser/StateCensusAnalyserTest.java
@@ -1,7 +1,5 @@
 package com.bridgelabz.statescensusanalyser;
 
-import java.io.IOException;
-
 import org.junit.Assert;
 import org.junit.Test;
 
@@ -9,10 +7,9 @@ public class StateCensusAnalyserTest {
 
 	public static final String CSV_FILE_PATH = "./StateCensus.csv";
 	public static final String NO_FILE = "./StateCensu.csv";
-	public static final String WRONG_FILE = "./State.csv";
 
 	@Test
-	public void GivenStateCensusCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
+	public void GivenStateCensusCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() {
 
 		int count = StateCensusAnalyser.loadDataToIterator(CSV_FILE_PATH);
 		Assert.assertEquals(29, count);
@@ -30,7 +27,17 @@ public class StateCensusAnalyserTest {
 	}
 
 	@Test
-	public void GivenStateCensusCSVFile_WithDelimiterIncorrect_ReturnsCensusAnalyserException() throws IOException {
+	public void GivenStateCensusCSVFile_WithDelimiterIncorrect_ReturnsCensusAnalyserException() {
+		try {
+			StateCensusAnalyser.loadDataToIterator(CSV_FILE_PATH);
+		} catch (CensusAnalyserException e) {
+			Assert.assertEquals(CensusAnalyserException.ExceptionType.TYPE_INCORRECT, e.type);
+			System.out.println(e.getMessage());
+		}
+	}
+	
+	@Test
+	public void GivenStateCensusCSVFile_IncorrectHeader_ReturnsCensusAnalyserException() {
 		try {
 			StateCensusAnalyser.loadDataToIterator(CSV_FILE_PATH);
 		} catch (CensusAnalyserException e) {
