c6c71c355e4f3fff781969776fce843b30530bfc
Swathi <swathi.h@perfios.com>
2021-09-21 18:01:23 +0530

    TC1.2: Incorrect file returning custom exception

--- src/test/java/com/bridgelabz/statescensusanalyser/StateCensusAnalyserTest.java
+++ src/test/java/com/bridgelabz/statescensusanalyser/StateCensusAnalyserTest.java
@@ -8,11 +8,23 @@ import org.junit.Test;
 public class StateCensusAnalyserTest {
 
 	public static final String CSV_FILE_PATH = "./StateCensus.csv";
+	public static final String NO_PATH = "./StateCensu.csv";
 
 	@Test
 	public void GivenStateCensusCsvFile_IfHasCorrectNumberOfRecords_ShouldReturnTrue() throws IOException {
+
 		int count = StateCensusAnalyser.loadDataToIterator(CSV_FILE_PATH);
 		Assert.assertEquals(29, count);
 
 	}
+	
+	@Test
+    public void GivenStateCensusCsvFile_IfInCorrect_ShouldThrowException() {
+        try {
+        	StateCensusAnalyser.loadDataToIterator(NO_PATH);
+        } catch (CensusAnalyserException e) {
+            Assert.assertEquals(CensusAnalyserException.ExceptionType.NO_SUCH_FILE, e.type);
+            System.out.println(e.getMessage());
+        }
+    }
 }
